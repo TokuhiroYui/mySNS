67ced276e8f374268d9eddfdde94e7010c16d694
徳弘佑衣 <y-tokuhiro@decoration-company.com>
2015-12-13 10:50:08 +0900

    Initial Commit

---
+++ MySNS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySNS
+//
+//  Created by 徳弘佑衣 on 2015/12/13.
+//  Copyright © 2015年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
